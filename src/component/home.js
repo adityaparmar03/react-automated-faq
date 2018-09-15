7b9ce7322e62ccb488dd87f3f7176e779af6f141
Pooja539 <32176046+Pooja539@users.noreply.github.com>
2018-09-14 17:56:19 -0700

    textbox voice control elements added

--- src/component/home.js
+++ src/component/home.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import '.'
 const Home = () => (
-    <div>
-    <h1>Hello from My Component</h1>
-    <h1>Hello from My Component</h1>
-    <h1>Hello from My Component</h1>
-    <h1>Hello from My Component</h1>
-    <h1>Hello from My Component</h1>
+    <div class="container">
+    <div class="row">
+            <div class="col">
+            </div>
+            <div class="col-8">
+                    <h2>Customer Support</h2>
+                    <div class="input-group mb-3">
+                            <input type="text" class="form-control" placeholder="Ask a question"  aria-label="Recipient's username" aria-describedby="basic-addon2"/>
+                            <div class="input-group-append">
+                                    <span class="input-group-text">  <i class="fa fa-microphone fa-microphone-local" aria-hidden="true"></i></span>
+                            </div>
+            
+                    </div>
+                            <button type="button" class="btn btn-primary" >Search</button>
+                            <button type="button" class="btn btn-primary" >Post</button>
+            </div>
+            <div class="col">
+            </div>
     </div>
+</div>
    
 
 );
